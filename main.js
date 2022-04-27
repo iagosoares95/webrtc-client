93050238f1e68112f9dbc59037c64ee7c633d7bd
iagosoares95 <iagosoaessantos@gmail.com>
2022-04-26 23:33:16 -0300

    Old project

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
 var userAgents = [];
 var sessions = [];
 
+var regUserAgent = {
+    userAgent: null,
+    inviteContext: null
+};
+var ctcUserAgent = {
+    userAgent: null,
+    inviteContext: null
+};
+
+var logger;
+
+var my_ip = "";
+
+function applog() {
+    for (var i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] == 'object') {
+            logger.innerHTML += ((JSON && JSON.stringify) ? JSON.stringify(arguments[i], undefined, 2) : arguments[i]) + ' ';
+        } else {
+            logger.innerHTML += arguments[i] + ' ';
+        }
+    }
+    logger.innerHTML += "</br>";
+}
+
+function onInvite(invitation) {
+    applog("ON INVITE");
+    regUserAgent.invitation = invitation;
+};
+
+
 var setupSession = function(session) {
     //https://sipjs.com/api/0.15.0/session/#events
 
     //HACK, remove and set this elsewhere
-    window.session = session;
-    sessions.push(session);
-
     session.on('progress', function(response) {
-        console.log("session on progress", session, response);
+        applog("Received session progress:", response.statusCode);
     });
 
     session.on('accepted', function(data) {
-        console.log("session on accepted", session, data);
+        applog("<b>Session accepted</b>", data.statusCode);
     });
 
     //called when a response 300-699 is received
     session.on('rejected', function(response, cause) {
-        console.log("session on rejected", session, response, cause);
+        applog("Session rejected: " + response.statusCode + "</b>");
         if (response.statusCode == 301 || response.statusCode == 302) {
             //TODO: check if Contact header is really there
             uri = response.headers.Contact[0].parsed.uri;
@@ -53,47 +80,86 @@ var setupSession = function(session) {
     });
 
     session.on('failed', function(response, cause) {
-        console.log("session on failed", session, response, cause);
+        applog("Session failed", response.statusCode, cause);
     });
 
     session.on('terminated', function(response, cause) {
-        console.log("session on terminated", session, response, cause);
+        applog("Session terminated", response.statusCode, cause);
     });
 
     session.on('cancel', function() {
-        console.log("session on cancel", session);
+        applog("<b>Session cancelled</b>");
     });
 
     session.on('reinvite', function(a_session) {
-        console.log("session on reinvite", session, a_session);
+        applog("Received a reinvite", a_session);
     });
 
     session.on('referRequested', function(context) {
-        console.log("session on referRequested", session, context);
+        applog("Received a refer", context);
     });
 
     session.on('replaced', function(newSession) {
-        console.log("session on replaced", session, newSession);
+        applog("Session was replaced", newSession);
     });
 
     session.on('dtmf', function(request, dtmf) {
-        console.log("session on dtmf", session, request, dtmf);
+        applog("DTMF digit received: ", dtmf);
     });
 
     session.on('SessionDescriptionHandler-created', function() {
-        console.log("session on SessionDescriptionHandler-created", session);
+        applog("Session description handler created");
     });
 
     session.on('directionChanged', function() {
-        console.log("session on directionChanged", session);
+        applog("Media direction changed: ", session.sessionDescriptionHandler.direction);
     });
 
     session.on('trackAdded', function() {
-        console.log("session on trackAdded", session);
+        applog("Track added to session");
     });
 
     session.on('bye', function(request) {
-        console.log("session on bye", session, request);
+        applog("<b>Session bye</b>", request);
+    });
+}
+
+var setupRegisterSession = function(session) {
+    session.once('registered', function() {
+        applog("Register successfull");
+        uaStatus.innerHTML = "Registered";
+    });
+
+    session.once('unregistered', function(cause) {
+        applog("User unregistered: " + (cause ? cause.statusCode : ""));
+        uaStatus.innerHTML = "Unregistered";
+    });
+
+    session.on('progress', function(response) {
+        applog("Received session progress:", response.statusCode);
+    });
+
+    session.on('accepted', function(data) {
+        applog("<b>Session accepted</b>");
+    });
+
+    session.on('rejected', function(data) {
+        applog("<b>Session rejected</b>");
+    });
+
+    session.on('invite', function(context) {
+        applog("<b>INVITE received</b>");
+        regUserAgent.inviteContext = context;
+        //var toAddr = inviteContext.request.to.toString();
+        // applog("<b>Call from:"<b>")
+    });
+
+    session.on('cancel', function() {
+        applog("<b>Session cancelled</b>");
+    });
+
+    session.on('bye', function(request) {
+        applog("<b>Session bye</b>", request);
     });
 }
 
@@ -112,12 +178,16 @@ var _configHandle = function(save) {
 
     var func = save == true ? save_func : load_func;
 
-    func(txtWebSocket, 'txtWebSocket');
-    func(txtURI, 'txtURI');
-    func(txtAuthorizationUser, 'txtAuthorizationUser');
-    func(txtDisplayName, 'txtDisplayName');
-    func(txtPassword, 'txtPassword');
-    console.log(JSON.stringify(localStorage, false, 4));
+    func(txtWebSocket, 'txtWebSocketSave');
+    func(txtURI, 'txtURISave');
+    func(txtAuthorizationUser, 'txtAuthorizationUserSave');
+    func(txtDisplayName, 'txtDisplayNameSave');
+    func(txtPassword, 'txtPasswordSave');
+    //applog(JSON.stringify(localStorage, false , 4));
+}
+
+var loadConfig = function() {
+    _configHandle(false);
 }
 
 var saveConfig = function() {
@@ -133,31 +203,44 @@ var loadFields = function() {
     if (!localStorage)
         return;
 
-    document.getElementById("txtWebSocket").value = localStorage.getItem("txtWebSocket");
-    document.getElementById("txtURI").value = localStorage.getItem("txtURI");
-    document.getElementById("txtAuthorizationUser").value = localStorage.getItem("txtAuthorizationUser");
-    document.getElementById("txtDisplayName").value = localStorage.getItem("txtDisplayName");
-    document.getElementById("txtPassword").value = localStorage.getItem("txtPassword");
+    document.getElementById("txtWebSocket").value = localStorage.getItem("txtWebSocketSave");
+    document.getElementById("txtURI").value = localStorage.getItem("txtURISave");
+    document.getElementById("txtAuthorizationUser").value = localStorage.getItem("txtAuthorizationUserSave");
+    document.getElementById("txtDisplayName").value = localStorage.getItem("txtDisplayNameSave");
+    document.getElementById("txtPassword").value = localStorage.getItem("txtPasswordSave");
+}
+
+async function get_ip() {
+    $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
+        console.log(JSON.stringify(data, null, 2));
+        my_ip = (data.ip);
+    });
 }
 
 window.onload = function() {
 
-    //var uaStatus = document.getElementById('ua-status');
-    var ua;
-    var userAgent = new SIP.UA(ua);
+    var enablevideooc = document.getElementById('enablevideooc');
+    var enablevideoic = document.getElementById('enablevideoic');
+    var enablevideoctc = document.getElementById('enablevideoctc');
 
+    logger = document.getElementById('log');
     loadFields();
+    my_ip = get_ip();
 
-    window.userAgent = userAgent;
-    userAgent.on('invite', function(session) {
-        setupSession(session);
-        session.accept();
-    });
+    /**
+     * Register handlers
+     */
+    function isRegisterActive() {
+        if (regUserAgent.userAgent == null || !regUserAgent.userAgent.isRegistered()) {
+            applog("<b>User agent is not registered</b>");
+            return false;
+        }
+        return true;
+    }
 
-    //Function from register button
     var regButton = document.getElementById('register');
     regButton.addEventListener('click', function() {
-        ua = {
+        var ua = {
             transportOptions: {
                 wsServers: [txtWebSocket.value],
                 traceSip: true,
@@ -170,95 +253,187 @@ window.onload = function() {
             level: 'debug',
             userAgentString: 'sipjs-X',
         };
-        userAgent = new SIP.UA(ua);
-        userAgent.register();
+        var registerUA = new SIP.UA(ua);
+        registerUA.register();
+        setupRegisterSession(registerUA);
+
+        var userContact = registerUA.contact.toString();
+        userContact = userContact.substr(1, userContact.length - 2);
+        applog("<b>Register sent for user: " + userContact + "</b>");
+
+        regUserAgent.userAgent = registerUA;
         saveConfig();
     });
 
-    //Function from untregister button
-    var desregButton = document.getElementById('unregister');
-    desregButton.addEventListener('click', function() {
-        userAgent.unregister();
+    var unregButton = document.getElementById('unregister');
+    unregButton.addEventListener('click', function() {
+
+        if (!isRegisterActive()) {
+            return;
+        }
+
+        var userContact = regUserAgent.userAgent.contact.toString();
+        userContact = userContact.substr(1, userContact.length - 2);
+        applog("<b>Unregistering user: " + userContact + "</b>");
+        regUserAgent.userAgent.unregister();
     });
-
-    //Function to end a call at Incoming call section
-    var endButtonic = document.getElementById('dropCallic');
-    endButtonic.addEventListener("click", function() {
-        session.bye();
-        //alert("Call Ended");
-    }, false);
-
-    //Function to end a call at Outgoing call section
-    var endButtonoc = document.getElementById('dropCalloc');
-    endButtonoc.addEventListener("click", function() {
-        session.bye();
-        //alert("Call Ended");
-    }, false);
-
-    //Function to end a call at Click to call section
-    var endButtonctc = document.getElementById('endCallctc');
-    endButtonctc.addEventListener("click", function() {
-        session.bye();
-        //alert("Call Ended");
-    }, false);
-
-    userAgent.on('registered', function() {
-        uaStatus.innerHTML = 'Registered';
-    });
-
-    userAgent.on('unregistered', function() {
-        uaStatus.innerHTML = 'Unregistered';
-    });
-
-    function onInvite(invitation) {
-        invitation.accept();
-    }
-
-    function onInvite(invitation) {
-        invitation.reject();
-    }
-
-    //makes a call at section Outgoing Call, only registered
-    var startButtonoc = document.getElementById("startCalloc");
-    startButtonoc.addEventListener("click", function() {
-        var session = userAgent.invite(txtTarget.value, {
-            sessionDescriptionHandlerOptions: {
-                constraints: {
-                    audio: true,
-                    video: false
-                }
-            }
-        });
-        setupSession(session);
-    }, false);
-
-    //makes a call at section Click to Call
-    var startButtonctc = document.getElementById("startCallctc");
-    startButtonctc.addEventListener("click", function() {
-        var session = userAgent.invite(txtTarget.value, {
-            sessionDescriptionHandlerOptions: {
-                constraints: {
-                    audio: true,
-                    video: false
-                }
-            }
-        });
-        setupSession(session);
-    }, false);
-
-    //Cancel the call at section Outgoing call
-    var cancelButtonoc = document.getElementById("cancelCalloc");
-    cancelButtonoc.addEventListener("click", function() {
-        session.terminate();
-    }, false);
 
     var checkRegButton = document.getElementById("checkRegister");
     checkRegButton.addEventListener("click", function() {
-        var estado = userAgent.isRegistered();
-        if (estado == true)
+
+        if (regUserAgent.userAgent.isRegistered())
             alert("User is registered!");
         else
             alert("User is unregistered");
     }, false);
 
+
+    /**
+     * Incoming call handlers, registered session only
+     */
+    var startButtonic = document.getElementById('startCallic');
+    startButtonic.addEventListener("click", function() {
+        if (!regUserAgent.inviteContext) {
+            applog("<b>No incoming call received.</b>");
+            return;
+        }
+        applog("<b>Accepting incoming call</b>");
+
+        regUserAgent.inviteContext.accept();
+    }, false);
+
+    var cancelButtonic = document.getElementById('cancelCallic');
+    cancelButtonic.addEventListener("click", function() {
+        if (!regUserAgent.inviteContext) {
+            applog("<b>No incoming call received.</b>");
+            return;
+        }
+
+        applog("<b>Rejecting incoming call</b>");
+        regUserAgent.inviteContext.reject();
+    }, false);
+
+    var endButtonic = document.getElementById('dropCallic');
+    endButtonic.addEventListener("click", function() {
+        if (!regUserAgent.inviteContext) {
+            applog("<b>No incoming call received.</b>");
+            return;
+        }
+
+        applog("<b>Dropping incoming call</b>");
+        regUserAgent.inviteContext.terminate();
+    }, false);
+
+
+    /**
+     * Outgoing call handlers, registered session only
+     */
+    var startButtonoc = document.getElementById("startCalloc");
+    startButtonoc.addEventListener("click", function() {
+        if (!isRegisterActive()) {
+            return;
+        }
+        var inviteContext = regUserAgent.userAgent.invite(txtTargetoc.value, {
+            sessionDescriptionHandlerOptions: {
+                constraints: {
+                    audio: true,
+                    video: enablevideooc.checked
+                }
+            }
+        });
+
+        var toAddr = inviteContext.request.to.toString();
+        toAddr = toAddr.substr(1, toAddr.length - 2);
+        applog("<b>Sending INVITE to: " + toAddr + "</b>");
+
+        setupSession(inviteContext);
+        regUserAgent.inviteContext = inviteContext;
+    }, false);
+
+    var cancelButtonoc = document.getElementById("cancelCalloc");
+    cancelButtonoc.addEventListener("click", function() {
+        if (!isRegisterActive()) {
+            return;
+        }
+        if (!regUserAgent.inviteContext) {
+            applog("<b>No INVITE context found</b>");
+            return;
+        }
+
+        regUserAgent.inviteContext.terminate();
+    }, false);
+
+
+    var endButtonoc = document.getElementById('dropCalloc');
+    endButtonoc.addEventListener("click", function() {
+        if (!isRegisterActive()) {
+            return;
+        }
+        if (!regUserAgent.inviteContext) {
+            applog("<b>No INVITE context found</b>");
+            return;
+        }
+
+        regUserAgent.inviteContext.bye();
+    }, false);
+
+
+    /**
+     * Click to call handlers, no register is used, so only outgoing calls
+     *  are possible
+     */
+    var startButtonctc = document.getElementById("startCallctc");
+    startButtonctc.addEventListener("click", function() {
+        txtws = txtWebServerctc.value.split('//');
+        txtws = txtws[1].split(':');
+        txtnumber = txtTargetctc.value.split('@');
+        target_call = txtnumber[0];
+        target_server = txtnumber[1];
+        my_name = txtYourNamectc.value + '@' + my_ip;
+
+        if (target_server == null) {
+            target_call = target_call.concat('@', txtws[0]);
+        }
+        var ua_ctc = {
+            transportOptions: {
+                wsServers: [txtWebServerctc.value],
+                traceSip: true,
+            },
+            uri: my_name,
+            contactName: my_name,
+            level: 'debug',
+            userAgentString: 'sipjs-X',
+        }
+        ctcUserAgent.userAgent = new SIP.UA(ua_ctc);
+        var inviteContext = ctcUserAgent.userAgent.invite(target_call, {
+            sessionDescriptionHandlerOptions: {
+                constraints: {
+                    audio: true,
+                    video: enablevideoctc.checked
+                }
+            }
+        });
+
+        var toAddr = inviteContext.request.to.toString();
+        toAddr = toAddr.substr(1, toAddr.length - 2);
+        applog("<b>Sending INVITE to: " + toAddr + "</b>");
+
+        setupSession(inviteContext);
+        ctcUserAgent.inviteContext = inviteContext;
+    }, false);
+
+    var endButtonctc = document.getElementById('endCallctc');
+    endButtonctc.addEventListener("click", function() {
+        if (!ctcUserAgent.inviteContext) {
+            return;
+        }
+        ctcUserAgent.inviteContext.terminate();
+        ctcUserAgent.inviteContext = null;
+    }, false);
+
+    // Function to clear the logs screen
+    var clearLogsButton = document.getElementById("clearLogs");
+    clearLogsButton.addEventListener("click", function() {
+        logger.innerHTML = "";
+    })
 }
